ea21ab348b0954406445996011be3f42673c4715
Yosra Emad <yosraemad14@gmail.com>
2022-08-17 13:33:37 +0200

    created icon, splash screen doesn't work

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,13 @@ import 'package:carrot_app/utils/palette.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
--- lib/views/login_page/login_page.dart
+++ lib/views/login_page/login_page.dart
@@ -1,9 +1,21 @@
 import 'package:carrot_app/views/login_page/widgets/header_image.dart';
 import 'package:carrot_app/views/login_page/widgets/login_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
