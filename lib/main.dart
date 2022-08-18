54bffd249c77badc966918ebf544c300c02c2da4
Yosra Emad <yosraemad14@gmail.com>
2022-08-18 18:11:10 +0200

    Revert "created icon, splash screen doesn't work"
    This reverts commit ea21ab348b0954406445996011be3f42673c4715.

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
--- lib/views/login_page/login_page.dart
+++ lib/views/login_page/login_page.dart
@@ -1,21 +1,9 @@
 import 'package:carrot_app/views/login_page/widgets/header_image.dart';
 import 'package:carrot_app/views/login_page/widgets/login_form.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
-  void initState() {
-    super.initState();
-    FlutterNativeSplash.remove();
-  }
 
   @override
   Widget build(BuildContext context) {
