9e04c4906da2c23dc0c3c5a8e4dd1d14d640e217
Yosra Emad <yosraemad14@gmail.com>
2022-08-21 12:32:58 +0200

    fixed splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:carrot_app/views/login_page/login_page.dart';
 import 'package:carrot_app/views/order_placed_page/order_placed_screen.dart';
 import 'package:carrot_app/views/signup_page/signup_page.dart';
 import 'package:carrot_app/utils/palette.dart';
-import 'package:carrot_app/views/splash_screen/splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -39,19 +38,17 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
             primarySwatch: PrimaryPalette.colors,
             scaffoldBackgroundColor: const Color(0xffFCFCFC)),
-        home: SplashScreen(
-          afterSplash: StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                UserViewModel()
-                    .signInUserWithId(context, snapshot.data!.uid)
-                    .then((value) => const HomePage());
-                return const HomePage();
-              }
-              return const LoginPage();
-            },
-          ),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              UserViewModel()
+                  .signInUserWithId(context, snapshot.data!.uid)
+                  .then((value) => const HomePage());
+              return const HomePage();
+            }
+            return const LoginPage();
+          },
         ),
         routes: {
           "/login": (context) => const LoginPage(),
